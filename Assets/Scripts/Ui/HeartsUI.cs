334ba70170c7eb0297e33398dbcab57d22fce2a4
AbbieK <abcool.abbiekoo@gmail.com>
2026-09-24 16:35:31 +0200

    Replace render-texture with sprite animation
    - baked from the 3D prefab into a spritesheet
    - Replace with an animated UI Image prefab
    - Adjust HeartsUI.cs

--- Assets/Scripts/Ui/HeartsUI.cs
+++ Assets/Scripts/Ui/HeartsUI.cs
@@ -5,14 +5,14 @@ using UnityEngine.UI;
 public class HeartsUI : MonoBehaviour
 {
     public static HeartsUI Instance;
-    public GameObject heartSlotPrefab;
-    public RawImage heartUIPrefab;
-    public Transform heartsRigParent;
+    public GameObject heartImagePrefab;
     public Transform heartsUIParent;
+
     public int maxHP = 3;
 
-    private HeartController[] hearts;
+    private GameObject[] hearts;
     private int currentHP;
+    private bool isDead;
 
     private void Awake()
     {
@@ -21,25 +21,13 @@ public class HeartsUI : MonoBehaviour
 
     private void Start()
     {
-        hearts = new HeartController[maxHP];
+        hearts = new GameObject[maxHP];
         currentHP = maxHP;
 
 
         for (int i = 0; i < maxHP; i++)
         {
-            GameObject rig = Instantiate(heartSlotPrefab, heartsRigParent);
-            rig.transform.localPosition = new Vector3(i * 5f, 0, 0);  //TODO: NEED TO TEST SINCE THERE IS ANIMATION
-
-            RenderTexture rt = new RenderTexture(256, 256, 16);
-            Camera cam = rig.GetComponentInChildren<Camera>();
-            cam.targetTexture = rt;
-
-            RawImage ui = Instantiate(heartUIPrefab, heartsUIParent);
-            ui.texture = rt;
-
-            HeartController controller = rig.AddComponent<HeartController>();
-            controller.Init(cam, ui.gameObject);
-            hearts[i] = controller;
+            hearts[i] = Instantiate(heartImagePrefab, heartsUIParent);
         }
     }
 
@@ -53,29 +41,29 @@ public class HeartsUI : MonoBehaviour
 
     }
 
-    private void FixedUpdate()
-    {
-        Die();
-    }
-
-
 
     public void RemoveHeart(int amount)
     {
+        if (isDead) return;
+
         for (int i = 0; i < amount; i++)
         {
             if (currentHP <= 0) break;
             currentHP--;
-            hearts[currentHP].Hide();
+            hearts[currentHP].SetActive(false);
         }
+
+        if (currentHP <= 0) Die();
     }
 
     public void AddHeart(int amount)
     {
+        if (isDead) return;
+
         for (int i = 0; i < amount; i++)
         {
             if (currentHP >= maxHP) break;
-            hearts[currentHP].Show();
+            hearts[currentHP].SetActive(true);
             currentHP++;
         }
     }
@@ -84,6 +72,9 @@ public class HeartsUI : MonoBehaviour
 
     public void Die()
     {
+        if (isDead) return;
+        isDead = true;
+
         if (hearts == null || currentHP == 0)
         {
             GameOver.Instance.Fail();
---
+++ Assets/Scripts/Ui/UIManagertest.cs
@@ -0,0 +1,266 @@
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+using Utility;
+using UnityEngine.UI;
+
+
+public class UIManagertest : SingletonBehaviour<UIManager>
+{
+    #region UI PANELS
+    [SerializeField] private GameObject mainMenuPanel;
+    [SerializeField] private GameObject inGameUIPanel;
+    [SerializeField] private GameObject pausePanel;
+    [SerializeField] private GameObject gameOverPanel;
+    [SerializeField] private GameObject youDiedPanel;
+    [SerializeField] private GameObject youWinPanel;
+
+    [SerializeField] private GameObject optionsPanel;
+    [SerializeField] private GameObject creditsPanel;
+    #endregion
+
+    #region INPUT ACTIONS
+    [SerializeField] private InputAction pauseAction;
+    #endregion
+
+    #region FAIL VARIABLES
+    [SerializeField] private TextMeshProUGUI timerText;
+    [SerializeField] private float startTime;
+    private float timeTillFail; //TODO: Need to confirm
+    //TODO: Need warning changing color/size/animation?
+    private bool isTimerRunning;
+    public event Action OnTimeUp;
+    #endregion
+
+    #region TOWER TRACKER
+    [SerializeField] private GameObject trackersPanel;
+    [SerializeField] private Slider playerSlider;
+    [SerializeField] private Slider lavaSlider;
+    [SerializeField] private TextMeshProUGUI heightText;
+    #endregion
+
+    #region SCORE
+    [SerializeField] private TextMeshProUGUI scoreText;
+    #endregion
+
+    public bool IsPaused { get; private set; }
+    public bool IsGameOver { get; private set; }
+
+    private void Start()
+    {
+        timeTillFail = startTime;
+        pauseAction.Disable();
+        inGameUIPanel.SetActive(false);
+        optionsPanel.SetActive(false);
+        creditsPanel.SetActive(false);
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    private void Update()
+    {
+        if (pauseAction.WasPressedThisFrame())
+        {
+            if (optionsPanel.activeSelf)
+                CloseOptionsButton();
+            else if (!IsGameOver)
+                Pause();
+        }
+
+        #region FAIL TIMER
+        timeTillFail -= Time.deltaTime;
+        int minutes = Mathf.FloorToInt(timeTillFail / 60f);
+        int seconds = Mathf.FloorToInt(timeTillFail % 60f);
+        int milliseconds = Mathf.FloorToInt((timeTillFail * 100f) % 60f);
+        timerText.text = $"{minutes:00}:{seconds:00}:{milliseconds:00}";
+
+        if (timeTillFail <= 0)
+        {
+            TriggerTimeUp();
+        }
+        #endregion
+
+        UpdateTrackers();
+    }
+
+    #region GAME STATE
+    public void Pause()
+    {
+        IsPaused = !IsPaused;
+
+        switch (IsPaused)
+        {
+            case true:
+                {
+                    Cursor.lockState = CursorLockMode.None;
+                    pausePanel.SetActive(true);
+                    Time.timeScale = 0;
+                    break;
+                }
+            case false:
+                {
+                    Cursor.lockState = CursorLockMode.Locked;
+                    pausePanel.SetActive(false);
+                    Time.timeScale = 1;
+                    break;
+                }
+        }
+    }
+    public void ResumeButton()
+    {
+        if (IsPaused) Pause();
+    }
+    public void MainMenuButton()
+    {
+        ResetGameState();
+        isTimerRunning = false;
+
+        mainMenuPanel.SetActive(true);
+        inGameUIPanel.SetActive(false);
+        gameOverPanel.SetActive(false);
+
+        pauseAction.Disable();
+        Cursor.lockState = CursorLockMode.None;
+        SceneManager.LoadScene(0);
+    }
+    public void RestartButton()
+    {
+        ResetTimer();
+        ResetGameState();
+
+        gameOverPanel.SetActive(false);
+        inGameUIPanel.SetActive(true);
+        trackersPanel.SetActive(false);
+
+        pauseAction.Enable();
+        Cursor.lockState = CursorLockMode.Locked;
+        SceneManager.LoadScene(1);
+    }
+
+    public void StartGame()
+    {
+        ResetTimer();
+        ResetGameState();
+
+        mainMenuPanel.SetActive(false);
+        gameOverPanel.SetActive(false);
+        inGameUIPanel.SetActive(true);
+
+        pauseAction.Enable();
+        Cursor.lockState = CursorLockMode.Locked;
+        SceneManager.LoadScene(1);
+    }
+
+    private void ResetGameState()
+    {
+        IsPaused = false;
+        IsGameOver = false;
+        Time.timeScale = 1;
+
+        pausePanel.SetActive(false);
+        youDiedPanel.SetActive(false);
+        youWinPanel.SetActive(false);
+    }
+
+    private void GameOverSceen(bool won)
+    {
+        IsGameOver = true;
+        isTimerRunning = false;
+
+        gameOverPanel.SetActive(true);
+        youDiedPanel.SetActive(!won);
+        youWinPanel.SetActive(won);
+    }
+    #endregion
+
+    #region FAIL METHODS
+    private void TriggerTimeUp()
+    {
+        OnTimeUp?.Invoke();
+        Fail();
+    }
+    public void Fail()
+    {
+        if (IsGameOver) return;
+        GameOverSceen(false);
+    }
+    public void Success()
+    {
+        if (IsGameOver) return;
+        GameOverSceen(true);
+        UpdateScoreDisplay();
+    }
+    public void ResetTimer()
+    {
+        timeTillFail = startTime;
+        isTimerRunning = true;
+        UpdateTimerText();
+    }
+    private void UpdateTimerText()
+    {
+        int minutes = Mathf.FloorToInt(timeTillFail / 60f);
+        int seconds = Mathf.FloorToInt(timeTillFail % 60f);
+        int milliseconds = Mathf.FloorToInt((timeTillFail * 100f) % 60f);
+        timerText.text = $"{minutes:00}:{seconds:00}:{milliseconds:00}";
+    }
+    #endregion
+
+    #region MENU BUTTONS
+    public void OptionsButton()
+    {
+        optionsPanel.SetActive(true);
+    }
+
+    public void CloseOptionsButton()
+    {
+        optionsPanel.SetActive(false);
+    }
+    public void CreditsButton()
+    {
+        creditsPanel.SetActive(true);
+    }
+    public void CloseCreditsButton()
+    {
+        creditsPanel.SetActive(false);
+    }
+    public void ExitButton()
+    {
+        //Need to look up
+    }
+    #endregion
+
+    #region SCORE
+    private void UpdateScoreDisplay()
+    {
+        if (scoreText == null) return;
+
+        int finalScore = CoinUI.Instance != null ? CoinUI.Instance.GetCoinCount() : 0;
+        scoreText.text = $"SCORE: {finalScore}";
+    }
+    #endregion
+
+    #region TRACKER METHODS
+    public void ShowTracker()
+    {
+        trackersPanel.SetActive(true);
+    }
+    private void UpdateTrackers()
+    {
+        if (TowerTracker.Instance == null || !trackersPanel.activeSelf) return;
+
+        playerSlider.value = TowerTracker.Instance.GetPlayerNormalizedHeight();
+        lavaSlider.value = TowerTracker.Instance.GetLavaNormalizedHeight();
+
+        float meters = TowerTracker.Instance.GetCurrentMeters();
+        heightText.text = $"{meters:F0}m";
+    }
+    #endregion
+
+    #region SINGLETON HANDLER
+    public override void Instantiate()
+    {
+    }
+    #endregion
+}
+
